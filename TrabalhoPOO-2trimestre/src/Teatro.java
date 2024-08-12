448ffc3871d332fe3be9aeac377f95c193fba034
Heitor de Sousa Freitas <heitorsf2007@gmail.com>
2024-08-12 13:11:45 -0300

    Update Teatro.java

--- TrabalhoPOO-2trimestre/src/Teatro.java
+++ TrabalhoPOO-2trimestre/src/Teatro.java
@@ -1,17 +1,34 @@
 import javax.swing.*;
 
 public class Teatro extends Evento {
-    public Teatro(String nome, String hora, String local, String data, double precoIngresso) {
+    Teatro(String nome, String hora, String local, String data, double precoIngresso) {
         super(nome, hora, local, data, precoIngresso);
-        this.quantiaIngressos = 50; // Exemplo de quantidade para teatro
+        super.quantiaIngressos = 250;
     }
 
     @Override
     public void add_I(Ingresso novo_I) {
-        if (assentos_Disp() > 0) {
-            this.ingressos_V.add(novo_I);
-        } else {
-            System.out.println("Sem assentos disponíveis para o evento: " + getNome());
+        if (ingressos_V.size() < quantiaIngressos) {
+            if (novo_I.tipo == 'M' && ingressos_MeiaDisp() <= quantiaIngressos * 0.2 && ingressos_MeiaDisp()>0) {
+                this.ingressos_V.add(novo_I);
+            } else if (novo_I.tipo == 'V' || novo_I.tipo == 'C') {
+                this.ingressos_V.add(novo_I);
+            } else if(novo_I.tipo == 'M' && ingressos_MeiaDisp() > quantiaIngressos * 0.2 ){
+                JOptionPane.showMessageDialog(null, "Os ingressos Meia-Entrada já esgotaram :(", "*ERRO*", JOptionPane.ERROR_MESSAGE);
+            }
+        }else{
+            JOptionPane.showMessageDialog(null, "Os ingressos para este evento já esgotaram :(", "*ERRO*", JOptionPane.ERROR_MESSAGE);
         }
     }
+
+    @Override
+    public double somaTotal_Receita() {
+        return 0;
+    }
+
+    @Override
+    public void extrato_Receita() {
+
+    }
 }
+
